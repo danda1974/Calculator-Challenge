60e09a41ff3e4b8ca5f131bb9713fc456dd531c5
Daniela Spacek <familie.spacek@googlemail.com>
2022-04-13 17:25:55 +0200

    created index.js draft

---
+++ index.js
@@ -0,0 +1,22 @@
+// PRESS ONE NUMBER
+// - GET THE NUMBER? 
+const buttonOneHTML = document.getElementById("buttonOne");
+
+let firstNumber;
+
+buttonOneHTML.addEventListener("click", () => {
+  // - SOMETHING TO HAPPEN WHEN CLICKED
+  // alert("THIS HAS BEEN CLICKED");
+  
+  // - STORE IT -> VARIABLE
+  firstNumber = 1;
+})
+
+
+// PRESS OPERATOR -> +
+// - 
+// PRESS SECOND NUMBER
+// - 
+// PRESS EQUALS -> =
+// -
+// DISPLAY RESULT
