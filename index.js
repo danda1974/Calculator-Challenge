ce00bb5eabd8d1bda5f33f11fa76fc1d104435c2
Daniela Spacek <familie.spacek@googlemail.com>
2022-04-17 14:28:35 +0200

    status update

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 // PRESS ONE NUMBER
 // - GET THE NUMBER? 
 const buttonOneHTML = document.getElementById("buttonOne");
-
 let firstNumber;
-
 buttonOneHTML.addEventListener("click", () => {
   // - SOMETHING TO HAPPEN WHEN CLICKED
-  // alert("THIS HAS BEEN CLICKED");
-  
+  // alert("THIS HAS BEEN CLICKED");  
   // - STORE IT -> VARIABLE
   firstNumber = 1;
 })
-
-
 // PRESS OPERATOR -> +
-// - 
+// - get the operation, add event listener and store it
+const buttonPlusHTML = document.getElementById("buttonPlus");
+let plusButton;
+buttonPlusHTML.addEventListener("click", () => {
+  plusButton = '+';
+})
 // PRESS SECOND NUMBER
-// - 
+const buttonTwoHTML = document.getElementById("buttonTwo");
+let secondNumber;
+buttonTwoHTML.addEventListener("click", () => {
+  secondNumber = 2;
+})
 // PRESS EQUALS -> =
-// -
+const buttonEqualsHTM = document.getElementById("buttonEquals");
+let equalsButton;
+buttonEqualsHTM.addEventListener("click", () => {
+  equalsButton = '=';
+})
 // DISPLAY RESULT
